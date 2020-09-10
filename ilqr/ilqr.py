36da01343d33b17ab31f911d3877365cae750a0a
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2020-09-10 17:58:49 +0200

    Basic working iLQR

--- ilqr/ilqr.py
+++ ilqr/ilqr.py
@@ -4,6 +4,10 @@ import pydrake.symbolic as sym
 
 from dynamics.glider import continuous_dynamics
 
+
+# Inspired by this
+# https://colab.research.google.com/github/RussTedrake/underactuated/blob/master/exercises/trajopt/ilqr_driving/ilqr_driving.ipynb#scrollTo=4IbLDqg7D
+
 # Discretize using forward Euler
 def discrete_dynamics(x, u):
     dt = 0.001
@@ -31,7 +35,7 @@ def rollout(x0, u_trj):
 def cost_stage(x, u):
     m = sym if x.dtype == object else np  # Check type for autodiff
 
-    goal = np.array([15, 0, 10])
+    goal = np.array([30, 0, 10])
     c_pos = (x[0:3] - goal).dot(x[0:3] - goal)
     c_control = (u[0] ** 2 + u[1] ** 2) * 0.1
     return c_pos + c_control
@@ -54,11 +58,6 @@ def cost_trj(x_trj, u_trj):
 
     total_cost += cost_final(x_trj[N - 1, :])
     return total_cost
-
-
-################
-# Numerical diff
-################
 
 
 class derivatives:
@@ -105,3 +104,118 @@ class derivatives:
         l_final_xx = sym.Evaluate(self.l_final_xx, env)
 
         return l_final_x, l_final_xx
+
+
+# Quadratic approximation of Q-function
+def Q_terms(l_x, l_u, l_xx, l_ux, l_uu, f_x, f_u, V_x, V_xx):
+    Q_x = l_x + f_x.T.dot(V_x)
+    Q_u = l_u + f_u.T.dot(V_x)
+    Q_xx = l_xx + f_x.T.dot(V_xx).dot(f_x)
+    Q_ux = l_ux + f_u.T.dot(V_xx).dot(f_x)
+    Q_uu = l_uu + f_u.T.dot(V_xx).dot(f_u)
+    return Q_x, Q_u, Q_xx, Q_ux, Q_uu
+
+
+def gains(Q_uu, Q_u, Q_ux):
+    Q_uu_inv = np.linalg.inv(Q_uu)
+    k = -Q_uu_inv.dot(Q_u)
+    K = -Q_uu_inv.dot(Q_ux)
+    return k, K
+
+
+def V_terms(Q_x, Q_u, Q_xx, Q_ux, Q_uu, K, k):
+    V_x = Q_x - K.T.dot(Q_uu).dot(k)
+    V_xx = Q_xx - K.T.dot(Q_uu).dot(K)
+    return V_x, V_xx
+
+
+def expected_cost_reduction(Q_u, Q_uu, k):
+    return -Q_u.T.dot(k) - 0.5 * k.T.dot(Q_uu.dot(k))
+
+
+def forward_pass(x_trj, u_trj, k_trj, K_trj):
+    x_trj_new = np.zeros(x_trj.shape)
+    x_trj_new[0, :] = x_trj[0, :]
+    u_trj_new = np.zeros(u_trj.shape)
+
+    for n in range(u_trj.shape[0]):
+        u_trj_new[n, :] = (
+            u_trj[n] + k_trj[n] + K_trj[n, :, :].dot(x_trj_new[n] - x_trj[n])
+        )
+        x_trj_new[n + 1, :] = discrete_dynamics(x_trj_new[n], u_trj_new[n])
+
+    return x_trj_new, u_trj_new
+
+
+def backward_pass(x_trj, u_trj, regu, derivs):
+    k_trj = np.zeros([u_trj.shape[0], u_trj.shape[1]])
+    K_trj = np.zeros([u_trj.shape[0], u_trj.shape[1], x_trj.shape[1]])
+    expected_cost_redu = 0
+    V_x, V_xx = derivs.final(x_trj[-1, :])
+
+    for n in range(u_trj.shape[0] - 1, -1, -1):
+        l_x, l_u, l_xx, l_ux, l_uu, f_x, f_u = derivs.stage(x_trj[n, :], u_trj[n, :])
+        Q_x, Q_u, Q_xx, Q_ux, Q_uu = Q_terms(
+            l_x, l_u, l_xx, l_ux, l_uu, f_x, f_u, V_x, V_xx
+        )
+        # Add regularization to ensure that Q_uu is invertible and nicely conditioned
+        Q_uu_regu = Q_uu + np.eye(Q_uu.shape[0]) * regu
+        k, K = gains(Q_uu_regu, Q_u, Q_ux)
+        k_trj[n, :] = k
+        K_trj[n, :, :] = K
+        V_x, V_xx = V_terms(Q_x, Q_u, Q_xx, Q_ux, Q_uu, K, k)
+        expected_cost_redu += expected_cost_reduction(Q_u, Q_uu, k)
+    return k_trj, K_trj, expected_cost_redu
+
+
+def run_ilqr(x0, n_x, n_u, N, max_iter=50, regu_init=100):
+    # First forward rollout
+    u_trj = np.random.randn(N - 1, n_u) * 0.0001
+    x_trj = rollout(x0, u_trj)
+    total_cost = cost_trj(x_trj, u_trj)
+    regu = regu_init
+    max_regu = 10000
+    min_regu = 0.01
+
+    # Setup traces
+    cost_trace = [total_cost]
+    expected_cost_redu_trace = []
+    redu_ratio_trace = [1]
+    redu_trace = []
+    regu_trace = [regu]
+
+    # Setup derivs
+    derivs = derivatives(discrete_dynamics, cost_stage, cost_final, n_x, n_u)
+
+    # Run main loop
+    for it in range(max_iter):
+        print("Iteration: " + str(it))
+        # Backward and forward pass
+        k_trj, K_trj, expected_cost_redu = backward_pass(x_trj, u_trj, regu, derivs)
+        x_trj_new, u_trj_new = forward_pass(x_trj, u_trj, k_trj, K_trj)
+        # Evaluate new trajectory
+        total_cost = cost_trj(x_trj_new, u_trj_new)
+        cost_redu = cost_trace[-1] - total_cost
+        redu_ratio = cost_redu / abs(expected_cost_redu)
+        # Accept or reject iteration
+        if cost_redu > 0:
+            # Improvement! Accept new trajectories and lower regularization
+            redu_ratio_trace.append(redu_ratio)
+            cost_trace.append(total_cost)
+            x_trj = x_trj_new
+            u_trj = u_trj_new
+            regu *= 0.7
+        else:
+            # Reject new trajectories and increase regularization
+            regu *= 2.0
+            cost_trace.append(cost_trace[-1])
+            redu_ratio_trace.append(0)
+        regu = min(max(regu, min_regu), max_regu)
+        regu_trace.append(regu)
+        redu_trace.append(cost_redu)
+
+        # Early termination if expected improvement is small
+        if expected_cost_redu <= 1e-6:
+            break
+
+    return x_trj, u_trj, cost_trace, regu_trace, redu_ratio_trace, redu_trace
--- main.py
+++ main.py
@@ -10,42 +10,56 @@ import pdb
 
 
 def main():
-    N = 10000
+    N = 1000
     n_x = 6
     n_u = 3
 
-    u_trj = np.zeros((N - 1, n_u))
     x0 = np.array([0, 0, 10, 100, 0, 50])
-    x_trj = rollout(x0, u_trj)
 
-    derivs = derivatives(discrete_dynamics, cost_stage, cost_final, n_x, n_u)
-
-    ###########
-    # Test rollout and costs
-    ###########
-
-    costs = np.zeros((N - 1, 1))
-    for i in range(N - 1):
-        costs[i] = cost_stage(x_trj[i], u_trj[i])
-
-    total_cost = cost_trj(x_trj, u_trj)
-    print(total_cost)
-
-    plt.plot(costs)
-    plt.title("costs")
-    plt.show()
-
+    max_iter = 10
+    regu_init = 100
+    x_trj, u_trj, cost_trace, regu_trace, redu_ratio_trace, redu_trace = run_ilqr(
+        x0, n_x, n_u, N, max_iter, regu_init
+    )
     ###########
     # Plotting
     ###########
+    # Analysis
+    plt.subplots(figsize=(10, 6))
+    plt.subplot(2, 2, 1)
+    plt.plot(cost_trace)
+    plt.xlabel("# Iteration")
+    plt.ylabel("Total cost")
+    plt.title("Cost trace")
+
+    plt.subplot(2, 2, 2)
+    delta_opt = np.array(cost_trace) - cost_trace[-1]
+    plt.plot(delta_opt)
+    plt.yscale("log")
+    plt.xlabel("# Iteration")
+    plt.ylabel("Optimality gap")
+    plt.title("Convergence plot")
+
+    plt.subplot(2, 2, 3)
+    plt.plot(redu_ratio_trace)
+    plt.title("Ratio of actual reduction and expected reduction")
+    plt.ylabel("Reduction ratio")
+    plt.xlabel("# Iteration")
+
+    plt.subplot(2, 2, 4)
+    plt.plot(regu_trace)
+    plt.title("Regularization trace")
+    plt.ylabel("Regularization")
+    plt.xlabel("# Iteration")
+    plt.tight_layout()
 
     fig = plt.figure()
     ax = fig.gca(projection="3d")
     x, y, z = np.meshgrid(
         # (-min, max, num steps)
-        np.arange(-10, 100, 10),
-        np.arange(-10, 100, 10),
-        np.arange(-10, 15, 5),
+        np.arange(-10, 40, 10),
+        np.arange(-10, 40, 10),
+        np.arange(0, 15, 3),
     )
     u, v, w = get_wind_field(x, y, z)
     ax.quiver(x, y, z, u, v, w, length=1, linewidth=1)
@@ -58,19 +72,23 @@ def main():
         linewidth=1,
     )
     ax.scatter(x0[0], x0[1], x0[2])
+    # goal = np.array([15, 0, 10])
+    ax.scatter(30, 0, 10)
     ax.legend()
     plt.show()
 
-    #    t = np.linspace(0, dt * N, num=N - 1)
-    #    fig, axs = plt.subplots(3)
-    #    fig.suptitle("Input")
-    #    axs[0].plot(t, u_sol[0, :])
-    #    axs[0].set_title("Input x")
-    #    axs[1].plot(t, u_sol[1, :])
-    #    axs[1].set_title("Input y")
-    #    axs[2].plot(t, u_sol[2, :])
-    #    axs[2].set_title("Input z")
-    #    plt.show()
+    dt = 0.001
+    t = np.linspace(0, dt * N, num=N - 1)
+    breakpoint()
+    fig, axs = plt.subplots(3)
+    fig.suptitle("Input")
+    axs[0].plot(t, u_trj[:, 0])
+    axs[0].set_title("Input x")
+    axs[1].plot(t, u_trj[:, 1])
+    axs[1].set_title("Input y")
+    axs[2].plot(t, u_trj[:, 2])
+    axs[2].set_title("Input z")
+    plt.show()
 
     return 0
 
