Fix sign error with wind models, as well as jacobian error

dynamics/wind_models.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 
+########
+# Exponential wind model
+########
+
+
 def exp_wind_model(z):  # Taken from Deittert et al.
     w_ref = 16  # m/s
     p = 0.143
@@ -18,21 +23,42 @@ def ddt_exp_wind_model(z, z_dot):
     return w_dot
 
 
+########
+# Logarithmic wind model
+########
+
+
 def log_wind_model(z):
-    w_ref = 10  # m/s
+    w_ref = 15  # m/s
     h_ref = 10  # m
     h_0 = 0.03  # m
-    if z < h_0: return 0 # NOTE zero wind below ground
+
+    if z < h_0:
+        return 0  # NOTE zero wind below ground
     w = w_ref * (np.log(z / h_0)) / (np.log(h_ref / h_0))
     return w
 
 
-def ddt_log_wind_model(z, z_dot):
+def ddz_log_wind_model(z):
+    w_ref = 15  # m/s
+    h_ref = 10  # m
+    h_0 = 0.03  # m
+
     dw_dz = w_ref / (np.log(h_ref / h_0) * z)
-    if z < h_0: dw_dz = 0 # NOTE zero wind below ground
-    w = w_ref * (np.log(z / h_0)) / (np.log(h_ref / h_0))
+    if z < h_0:
+        dw_dz = 0  # NOTE zero wind below ground
+    return dw_dz
+
+
+def ddt_log_wind_model(z, z_dot):
+    dw_dz = ddz_log_wind_model
     dw_dt = dw_dz * z_dot
     return dw_dt
+
+
+########
+# Logistic wind model
+########
 
 
 def logistic_wind_model(z):  # Taken from slotine
@@ -45,7 +71,9 @@ def logistic_wind_model(z):  # Taken from slotine
 def ddz_logistic_wind_model(z):
     w_freestream = 16  # Free stream wind speed
     delta = 5  # wind_shear_layer thickness
-    dw_dz = (w_freestream * np.exp(-z / delta)) / (delta * (1 + np.exp(-z / delta)) ** 2)
+    dw_dz = (w_freestream * np.exp(-z / delta)) / (
+        delta * (1 + np.exp(-z / delta)) ** 2
+    )
     return dw_dz
 
 
@@ -55,6 +83,10 @@ def ddt_logistic_wind_model(z, z_dot):
     return w_dot
 
 
+########
+# General functions
+########
+
 
 def get_wind_vector(z):
     w_vec = np.array([0, -wind_model(z), 0])
@@ -62,20 +94,14 @@ def get_wind_vector(z):
 
 
 def get_wind_jacobian(z):
-    dw_dz = ddz_logistic_wind_model(z)
-    dw_dx = np.array([[0, 0, 0], [0, 0, dw_dz], [0, 0, 0]])
+    dw_dz = ddz_wind_model(z)
+    dw_dx = np.array([[0, 0, 0], [0, 0, -dw_dz], [0, 0, 0]])
     return dw_dx
 
 
-#wind_model = logistic_wind_model
-#ddt_wind_model = ddt_logistic_wind_model
-
 wind_model = log_wind_model
+ddz_wind_model = ddz_log_wind_model
 ddt_wind_model = ddt_log_wind_model
-
-#wind_model = exp_wind_model
-#ddt_wind_model = ddt_exp_wind_model
-
 
 # PLOTTING FUNCTIONs
 def plot_log_wind_model(z):
@@ -84,6 +110,7 @@ def plot_log_wind_model(z):
     h_0 = 0.03  # m
     w = w_ref * (np.log(z / h_0)) / (np.log(h_ref / h_0))
     return w
+
 
 plot_wind_model = plot_log_wind_model
 
@@ -94,4 +121,3 @@ def get_wind_field(x, y, z):
     w = np.zeros(z.shape)
 
     return u, v, w
-